7c26b5bf888800ea1f38fc2582f512ee0c04c402
johnbetaro <johnbetaro_github@proton.me>
2025-03-16 13:54:20 +0100

    Delete app directory

--- app/src/main/java/org/thoughtcrime/securesms/main/Material3OnScrollHelperBinder.kt
+++
@@ -1,8 +0,0 @@
-package org.thoughtcrime.securesms.main
-
-import androidx.recyclerview.widget.RecyclerView
-
-interface Material3OnScrollHelperBinder {
-  fun bindScrollHelper(recyclerView: RecyclerView)
-  fun bindScrollHelper(recyclerView: RecyclerView, chatFolders: RecyclerView, setChatFolder: (Int) -> Unit)
-}
